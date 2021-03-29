b3d00ad9e9e1005387273df3ea13565b7a5d3e05
maciej@fly <baranskimaciej1984@gmail.com>
2021-03-29 16:03:11 +0200

    load loop

--- mb/__init__.py
+++ mb/__init__.py
@@ -6,6 +6,9 @@ import pkgutil
 
 __all__ = []
 for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
+    if module_name == 'tf_utils': # dont load it automatically (cuda stuff dependent etc) use import mb.tf_utils
+        continue
+    print(module_name)
     __all__.append(module_name)
     _module = loader.find_module(module_name).load_module(module_name)
     globals()[module_name] = _module
